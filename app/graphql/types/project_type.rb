a5f3e3f727353bfb57a0697be53bac52565bcbba
Chase Gaddis <cgaddis36@gmail.com>
2022-10-31 16:18:01 -0500

    add new type for Project model

---
+++ app/graphql/types/project_type.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Types
+  class ProjectType < Types::BaseObject
+    field :id, ID, null: false
+    field :name, String
+    field :url, String
+    field :github, String
+    field :description, String
+    field :frameworks, [String]
+    field :hosts, [String]
+    field :created_at, GraphQL::Types::ISO8601DateTime, null: false
+    field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
+  end
+end
