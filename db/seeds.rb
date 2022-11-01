4b690c2d85e87d710ba5221fc48861d4cc70ec34
Chase Gaddis <cgaddis36@gmail.com>
2022-11-01 17:55:14 -0500

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,97 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+project0 = Project.create(
+  name: 'ShoreCasts',
+  classification: 'Personal Project',
+  url: 'www.shorecasts.com',
+  frameworks: ['Rails', 'GraphQL', 'React'],
+  hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
+  description: 'Designed to centralize the location 
+    for all of the necessary forecasting tools that recreational boaters 
+    and saltwater anglers utilize to plan their days on the water.',
+  github: 'www.shorecasts.com',
+  features: ['User Login', 
+  'Fishing Store Locator', 
+  'Fishing Guides Locator', 
+  'Hourly Weather Predictions', 
+  'Daily Weather Predictions', 
+  'Wind Forecast', 
+  'Water Temperature recordings', 
+  'Tidal Height Predictions', 
+  'Mailer'],
+  apis: ['NOAA Tides & Currents', 'National Weather Service', 'Custom API built designed for Fishing Stores & Guides']
+)
+project1 = Project.create(
+  name: 'Leap Claims Form',
+  classification: 'Work Project',
+  url: 'https://www.leapeasy.com/claim-form/',
+  github: 'https://www.leapeasy.com/claim-form/',
+  frameworks: ['Rails', 'GraphQL', 'React'],
+  hosts: ['AWS Amplify', 'Elastic Beanstalk', 'RDS database'],
+  description: 'Claims form designed to help landlords that work 
+    with Leap file insurance claims on their policies.',
+  apis: ['Salesforce'],
+  features: ['Google Places Autocomplete', 'Salesforce Automation', 'Salesforce Record Mutations']
+)
+project2 = Project.create(
+  name: 'Bluesman Charters',
+  classification: 'Personal Project',
+  url: 'www.bluesmancharters.com',
+  github: 'https://github.com/cgaddis36/bluesman-charter',
+  hosts: ['AWS Amplify'],
+  frameworks: ['React'],
+  features: ['Informational Webpage', 
+  'PWA scaled for mobile and web use'],
+  description: 'Progressive web application designed to help a 
+  local charter boat business in Destin, FL easily book charters.'
+)
+
+project3 = Project.create(
+  name: 'Project Nexus',
+  classification: 'Work Project',
+  url: 'www.promenade.ai',
+  github: 'www.promendae.ai',
+  hosts: ['Heroku'],
+  frameworks: ['Rails', 'GraphQL', 'React'],
+  apis: ['Constructed Api'],
+  features: ['Admin Dashboard', 
+  'Custom Location Finder', 
+  'Stripe Payment Checkout'],
+  description: 'Microservice designed to help veterans locate 
+  specific medical services to facilitate their navigation of 
+  post-military life.'
+)
+
+project4 = Project.create(
+  name: 'Portfolio Site',
+  classification: 'Personal Project',
+  url: 'www.chasegaddis.com',
+  github: 'https://github.com/cgaddis36/portfolio-site',
+  hosts: ['AWS Amplify', 'AWS Elastic Beanstalk', 'RDS Database'],
+  frameworks: ['Rails', 'GraphQL', 'React'],
+  apis: ['Constructed Api'],
+  features: ['Unique Database Construction'],
+  description: 'Web application designed to showcase development projects.'
+)
+
+project5 = Project.create(
+  name: 'Gift of Gab',
+  classification: 'School Project',
+  url: 'https://gift-of-gab-deployed.herokuapp.com/',
+  github: 'https://github.com/GiftOfGab1',
+  hosts: ['Heroku'],
+  frameworks: ['Rails', 'GraphQL', 'Flask', 'React'],
+  apis: ['Constructed Api', 'Text To Speech'],
+  features: ['Text to Audio', 'Microservice', 'redux'],
+  description: 'Progressive web application designed to facilitate speech for individuals with cognitive disabilities.'
+)
+
+project6 = Project.create(
+  name: 'Local Spotlight',
+  classification: 'School Project',
+  url: 'https://polar-caverns-76159.herokuapp.com/',
+  github: 'https://github.com/cgaddis36/local_spotlight',
+  hosts: ['Heroku'],
+  frameworks: ['Rails'],
+  apis: ['Spotify'],
+  features: ['Spotify Oauth', 'Geographical Location mapping'],
+  description: 'Web application designed to locate local up and coming artists for you based on geographical location.'
+)
