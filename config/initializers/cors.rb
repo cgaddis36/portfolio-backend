1545c714792b17d80738e1c5b5ee917fd00a998a
Chase Gaddis <cgaddis36@gmail.com>
2022-11-02 12:08:54 -0500

    add FE to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,9 +8,11 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "http://localhost:3000",
-    'https://loaclhost:3000',
+    'https://localhost:3000',
     'http://192.168.0.208:3000',
-    'https://192.168.0.208:3000'
+    'https://192.168.0.208:3000',
+    'http://chasegaddis.com',
+    'https://chasegaddis.com'
 
 
     resource "*",
