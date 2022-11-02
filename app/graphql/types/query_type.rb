2016338079d644970fa93a0babf7278ed1c6018f
Chase Gaddis <cgaddis36@gmail.com>
2022-11-02 17:07:22 -0500

    add fields for returning jobs and institutions

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,5 +2,7 @@ module Types
   class QueryType < Types::BaseObject
     field :find_project, resolver: Queries::FindProject
     field :projects, resolver: Queries::Projects 
+    field :institutions, resolver: Queries::Institutions
+    field :jobs, resolver: Queries::Jobs
   end
 end
