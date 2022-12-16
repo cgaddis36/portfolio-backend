426e7f0950c0172ae2771e0606312903d9585002
Chase Gaddis <cgaddis36@gmail.com>
2022-12-16 16:41:28 -0600

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ developer = Developer.create(
   github: "https://github.com/cgaddis36",
   title: "Software Developer",
   background: "My main goal as a developer is to build highly personable applications designed to optimize efficiency and create solutions to real world problems.
-  Innovation is what drives me in every project I build. Whether it be developing new services for a startup to help Veterans navigate post military life 
+  Innovation & collaboration are what drive me in every project I build. Whether it be developing new services for a startup to help Veterans navigate post military life 
   or bringing my own ideas to life, creating new products to solve business problems and provide widespread accessibility to arbitrary data is what greases
   my gears as a developer. When I am not coding, you can find me throwing the ball with my labrador retriever Lilly in one of the local parks or fishing for 
   speckled sea trout & redfish from a kayak. I would be hard pressed to find a better day than hiking to a mountain stream and fly fishing all day."
